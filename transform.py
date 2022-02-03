0d4283ec45c40dd03666fdea346608861ff9192b
Bhall1234 <ben.hall17@outlook.com>
2022-02-03 16:28:26 +0000

    removed rubbish

--- transform.py
+++ transform.py
@@ -1,25 +1,6 @@
 
 import cv2
 import numpy as np
-#%matplotlib qt5
-#import matplotlib.pyplot as plt
- 
-# To open matplotlib in interactive mode
-#%matplotlib qt5
- 
-# Load the image
-#img = cv2.imread('Photos/board.png') 
- 
-# Create a copy of the image
-#img_copy = np.copy(img)
- 
-# Convert to RGB so as to display via matplotlib
-# Using Matplotlib we can easily find the coordinates
-# of the 4 points that is essential for finding the 
-# transformation matrix
-#img_copy = cv2.cvtColor(img_copy,cv2.COLOR_BGR2RGB)
- 
-#plt.imshow(img_copy)
 
 # Load the image
 image = cv2.imread('Photos/board.png') 
